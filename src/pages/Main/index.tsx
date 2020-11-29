e93ebcfebb033c7c3d43e8e63e256f7db84d9454
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-11-29 07:07:02 -0300

    feat: add main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Container, Content, Title } from './styles'
 
 const Main: React.FC = () => {
+  const [word, setWord] = useState('')
+
+  const addWord = useCallback((chars: string[]) => {
+    chars.map((char, index) => setTimeout(() => {
+        setWord(prevState => prevState + char)
+      }, [1000 + 200 + index * 200])
+    )
+  }, [])
+
+  useEffect(()=> {
+    addWord(['I', 'n', 'd', 'i', 'e', ' ', 'C', 'a', 'm', 'p', 'e', 'r', 's', ' ', ':', ')'])
+  }, [])
+
   return (
-    <div>Você está na main</div>
+    <Container>
+      <Content>
+        <Title>
+          <h2>hello, <span>{word}</span></h2>
+          <div/>
+        </Title>
+        
+        <h1>eu sou Agnaldo Burgo Junior,</h1>
+        <p>engenheiro e desenvolvedor de software. Este é meu portfólio especial para vocês!</p>
+        <button type='button'>Eu na Indie Campers...</button>
+      </Content>
+    </Container>
   );
 };
 
---
+++ src/pages/Main/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  width: 1140px;
+  margin: 0 auto;
+  font-family: 'Roboto Slab', serif;
+`
+
+export const Content = styled.div`
+  margin-top: 100px;
+  max-width: 550px;
+
+  h2 {
+    font-size: 28px;
+  }
+
+  h1 {
+    font-size: 36px;
+    font-weight: 700;
+    margin-bottom: 28px;
+  }
+
+  p {
+    font-family: 'Montserrat', sans-serif;
+    line-height: 1.2;
+    margin-bottom: 24px;
+  }
+
+  button {
+      background: #ff5900;
+      height: 48px;
+      border-radius: 4px;
+      border: 0;
+      padding: 12px 60px;
+      color: #fff;
+      font-weight: 700;
+      margin-top: 16px;
+      margin-bottom: 40px;
+      transition: background-color 0.2s;
+      cursor: pointer;
+
+      &:hover {
+        background: ${shade(0.2, '#ff5900')};
+      }
+    }
+`
+
+
+export const Title = styled.div`
+  font-weight: 600;
+
+  span {
+    color: #5440cc;
+  }
+`
